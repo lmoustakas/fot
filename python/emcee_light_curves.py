ac3d6e64f0fc8673a51a45080ac433e75a0bd356
Andres Romero-Wolf <afromero@afromero>
2014-04-02 12:07:19 -0700

    fixed bug in emcee, it is now giving more reasonable results

--- python/emcee_light_curves.py
+++ python/emcee_light_curves.py
@@ -32,7 +32,15 @@ def lnprior(theta):
   if 1.e-10<sigma<1.e10 and 1.e-10<tau<1.e10 and -1000.<b<1000.:
         return 0.0
   return -np.inf
-    
+
+#test lnprior
+'''
+for k in arange(-1.e4,1.e4, 1.e2):
+  theta = [0.56, 121, k]
+  print k,lnprior(theta)
+exit()
+'''
+
 def lnprob(theta, t, lc, err):
     lp = lnprior(theta)
     if not np.isfinite(lp):
@@ -40,8 +48,9 @@ def lnprob(theta, t, lc, err):
     return lp + kelly_ll(theta, t, lc, err)
 #M_BH = 10**(7.78/8.) #in 10^8 M_sun
 #R = 1./(1.1*M_BH)*121 # in 100 R_suns
+
 z = 0.658 # redshift
-b=-1.0
+b=0.0
 tau=121.
 sigma=0.56 #for RX J1131 at 1000 A
 
@@ -64,39 +73,60 @@ xlabel('time, days')
 ylabel('Rel. Mag.')
 
 '''
-# NUMERICALLY OPTIMIZE THE SOLUTION
-nll = lambda *args: -kelly_delay_ll_3(*args)
-#result = op.minimize(nll, [m_true, b_true, np.log(f_true)], args=(x, y, yerr))
-result = op.fmin(nll, [delay, log10(sigma), log10(tau), b], args=(t, lc1, err1, lc2, err2))
-#m_ml, b_ml, lnf_ml = result["x"]
-delay_ml, log10_sigma_ml, log10_tau_ml, b_ml = result
-if(result[2]<0.): result[2]*=-1.
-
-print 'delay=%1.2e\tdelay_ml=%1.2e'%(delay,delay_ml)
-print 'log10_sigma=%1.2e\tlog10_sigma_ml=%1.2e'%(log10(sigma),log10_sigma_ml)
-print 'log10_tau=%1.2e\tlog10_tau_ml=%1.2e'%(log10(tau),log10_tau_ml)
-print 'b=%1.2e\tb_ml=%1.2e'%(b,b_ml)
+#test lnprob
+#b_array=concatenate([-10**arange(5.,-5.,-1.),10**arange(-5.,5.,1.)])
+b_array=arange(-100.,100.)
+figure()
+for b_val in b_array:
+  theta = [sigma, tau, b_val]
+  print b_val,lnprob(theta, t, X_data, error*ones(len(X_data)))
+  plot([b_val],[lnprob(theta, t, X_data, error*ones(len(X_data)))] ,'k.')
+show()
+exit()
 '''
+
+# NUMERICALLY OPTIMIZE THE SOLUTION
+print 'NUMERICAL OPTIMIZATION OF THE SOLUTION'
+nll = lambda *args: -kelly_ll(*args)
+#result = op.minimize(nll, [m_true, b_true, np.log(f_true)], args=(x, y, yerr))
+result = op.fmin(nll, [sigma, tau, b], args=(t, X_data, error*ones(len(X_data))))
+#m_ml, b_ml, lnf_ml = result["x"]
+sigma_ml, tau_ml, b_ml = result
+#if(result[2]<0.): result[2]*=-1.
+
+print 'sigma=%1.2e\tsigma_ml=%1.2e'%((sigma),sigma_ml)
+print 'tau=%1.2e\ttau_ml=%1.2e'%((tau),tau_ml)
+print 'b=%1.2e\tb_ml=%1.2e'%(b,b_ml)
+#exit()
+
 #RUN EMCEE
-#ndim, nwalkers = 3, 100
-ndim, nwalkers = 3, 10
+ndim, nwalkers = 3, 100
+#ndim, nwalkers = 3, 10
 print 'pos'
 true_vals=[sigma, tau, b]
-pos = [true_vals + 1e-4*np.random.randn(ndim) for i in range(nwalkers)]
+#print 1e-4*np.random.randn(ndim)
+pos = [true_vals + 1e-3*np.random.randn(ndim) for i in range(nwalkers)]
+#print pos
 print 'sampler'
-sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, args=(t, X, error*ones(len(X))))
+sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, args=(t, X_data, error*ones(len(X))))
 print 'burn-in sampler.run_mcmc'
 #sampler.run_mcmc(pos, 500)
 sampler.run_mcmc(pos, 100)
 sampler.reset()
 print 'sampler.run_mcmc'
-#sampler.run_mcmc(pos, 1000)
-sampler.run_mcmc(pos, 100)
+sampler.run_mcmc(pos, 1000)
+#sampler.run_mcmc(pos, 100)
+#for i in range(ndim):
+    #figure()
+    #hist(sampler.flatchain[:,i], 100, color="k", histtype="step")
+    #title("Dimension {0:d}".format(i))
+#show()
 print 'sampler.chain'
 samples = sampler.chain[:, 50:, :].reshape((-1, ndim))
 print("Mean acceptance fraction: {0:.3f}".format(np.mean(sampler.acceptance_fraction)))
-
-samples[:, 2] = np.exp(samples[:, 2])
+#exit()
+#PROBLEM MUST BE HERE!
+#samples[:, 2] = np.exp(samples[:, 2])
 mc_sigma, mc_tau, mc_b = map(lambda v: (v[1], v[2]-v[1], v[1]-v[0]), zip(*np.percentile(samples, [16, 50, 84],axis=0)))
 
 print 'Errors based on 16th, 50th, and 84th percentile'
