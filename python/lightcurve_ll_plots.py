3f2e087a1b022ba2cac6e654b2ae4280a5cffb51
Andres Romero-Wolf <afromero@afromero>
2014-04-01 20:07:22 -0700

    updates qsr_ll.py and light_curve_ll_plots.py

--- python/lightcurve_ll_plots.py
+++ python/lightcurve_ll_plots.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 from pylab import *
+import random
 import numpy as np
-from drw_lightcurve import *
-from qsr_ll import hojjati_ll
+from drw_lightcurve import drw_lightcurve
+from hubble_observation_schedule import hubble_obs_sched
+#from qsr_ll import hojjati_ll
 from qsr_ll import kelly_ll
 import time
 
@@ -12,7 +14,7 @@ rcParams['axes.formatter.limits']=-4,4
 rcParams['figure.facecolor'] = 'white'
 rcParams['figure.subplot.hspace']=0.7
 rcParams['figure.subplot.wspace']=0.5
-rcParams['figure.subplot.left']=0.1
+rcParams['figure.subplot.left']=0.15
 rcParams['figure.subplot.right']=0.9
 rcParams['figure.subplot.top']=0.88
 rcParams['figure.subplot.bottom']=0.1
@@ -22,116 +24,127 @@ rcParams['lines.markersize'] = 10
 rcParams['lines.linewidth'] = 1
 rcParams['lines.markeredgewidth'] = 1
 
-M_BH = 10**(7.78/8.) #in 10^8 M_sun
-R = 1./(1.1*M_BH)*121 # in 100 R_suns
+#M_BH = 10**(7.78/8.) #in 10^8 M_sun
+#R = 1./(1.1*M_BH)*121 # in 100 R_suns
 z = 0.658 # redshift
-
-X_0=0. #initial condition of light curve
-#tau=1.1 * M_BH * R
+b=-1.0
 tau=121.
-#tau=1.e-5
-print 'tau',tau
-b=0.0
 sigma=0.56 #for RX J1131 at 1000 A
-#sigma=1.e-3 #for RX J1131 at 1000 A
+
+X_0=b*tau #initial condition of light curve
+print 'tau',tau
 Nsteps=1000
 Nsamples=1000
 
-X=[0.]
-t_sample=[0.]
-t=[0.]
-T_orbit=90. #minutes
-t_obs=0.6   #fraction of orbit where source is visible
-t_int=20. # minutes
-dt=90./(60.*24)
-counter=0
-bail=0
-while (bail==0):
-  counter+=1
-  t_val=t_sample[len(t_sample)-1]+20.+(random.gauss(0.,5.)) #in minutes
-  #print t_val, mod(t_val,90.), t_obs*T_orbit
-  t_sample.append(t_val)
-  if(mod(t_val,90.)<t_obs*T_orbit):
-    t.append(t_val)
-  if(t_val>80.*90.): bail=1
-t=array(t)/(60.*24.)
-
-figure(1)
-plot(t, ones(len(t)), '.')
-plot(array(t_sample)/(60.*24.), 2.*ones(len(t_sample)), '.')
-xlabel('time, days')
-ylim(0.,3.)
-#show()
-#exit() 
-
-#t/=(1+z) #time at the rest frame of the quasar
-for k in range(1,len(t)):
-  #t.append(t[k-1]+1.+abs(random.gauss(0.,dt)))
-  #t.append(t[k-1]+dt)
-  X.append(X_int(t[k]-t[k-1], X[k-1], tau, b, sigma, Nsteps))
-X=array(X)
-error=0.015
+t=hubble_obs_sched(80)
+#figure(1)
+#subplot(211)
+#plot(t, ones(len(t)), '.')
+#xlabel('time, days')
+#title('Hubble-like obs. schedule, 80 orbits')
+#ylim(0.,2.)
+#xlim(0.,max(t))
+#subplot(212)
+#plot(t, ones(len(t)), '.')
+#xlabel('time, days')
+#xlim(0.,1.)
+#ylim(0.,2.)
+#title('Hubble-like obs. schedule, 1 day zoom in')
+X = drw_lightcurve(t, X_0, tau, sigma, b, z, Nsteps)
+error=0.02
 err=array([random.gauss(0.,error) for _ in xrange(len(X))])
 X_data=X+err
 #t*=(1+z) # convert back to time at the observer
 
-figure(2)
+figure()
 subplot(111)
-errorbar(t,X_data,yerr=error, fmt='k.', label='1.5% error')
+errorbar(t,X_data,yerr=error, fmt='k.', label='2% error')
 plot(t,X, 'rs', ms=4, mec='r', label='Light Curve')
 legend(loc=2)
 xlabel('time, days')
 ylabel('Rel. Mag.')
+#show()
 
-t_avg=[]
-for k in range(0,len(t)-1):
-  t_avg.append(0.5*(t[k+1]+t[k]))
-figure(3)
-subplot(211)
-plot(t_avg,diff(X_data), '.')
-plot(t_avg,diff(X), '.')
-subplot(212)
-hist(diff(X_data), bins=25)
-hist(diff(X), bins=25)
-
-#M = np.identity(3)
-#tau_array=arange(1.,300.,3.)
-tau_array=10**arange(-5.,5.,0.1)
-#sigma_array=arange(0.1,1.0,0.1)
-#sigma_array=10**arange(-3,3,0.5)
+#SIGMA DEPENDENCE
+figure()
+tau_array=tau*10**arange(-5.,5.,1.)
+#sigma_array=array([0.0056, 0.056,0.56, 5.6, 56.])
 sigma_array=10**arange(-3,3,0.1)
-#Nsamp=len(X)
-#Nsamp=60
-print 'len(X)',len(X)
 Ntau=len(tau_array)
 Nsig=len(sigma_array)
-#Xg, Yg=np.meshgrid(sigma_array, tau_array)
-Xg, Yg=np.meshgrid(log10(array(sigma_array)), log10(array(tau_array)))
-Z = Xg*Yg
-#M = 0.*np.random.rand(Ntau*Nsig, Nsamp,Nsamp)
-figure(4)
 max_val=0.
+for i in range(0,Ntau):
+  ll=[]
+  for j in range(0,Nsig):
+    #val = hojjati_ll(t, X_data, err, sigma_array[j], tau_array[i], 0.)
+    val = kelly_ll([sigma_array[j], tau_array[i], b], t, X_data, err)
+    ll.append(val)
+  max_val=max(max_val,max(ll))
+  #print len(sigma_array), len(ll)
+  if(i<=6): semilogx(sigma_array,ll, '+-',lw=2, label=r'$\tau$=%1.2e'%(tau_array[i]))
+  if(i>6): semilogx(sigma_array,ll, '+--', lw=3, label=r'$\tau$=%1.2e'%(tau_array[i]))
+  ylim(-10.*max_val,2.*max_val)
+  xlabel(r'sigma, mag day$^{-1/2}$')
+  ylabel('log likelihood')
+  title('True tau=%1.2e'%(tau))
+legend(loc=3)
+#show()
 
+
+#TAU DEPENDENCE
+figure()
+tau_array=10**arange(-5.,5.,0.1)
+#sigma_array=array([0.0056, 0.056,0.56, 5.6, 56.])
+sigma_array=sigma*10**arange(-2.,2.5,0.5)
+Ntau=len(tau_array)
+Nsig=len(sigma_array)
+max_val=0.
 for j in range(0,Nsig):
   ll=[]
   for i in range(0,Ntau):
     #val = hojjati_ll(t, X_data, err, sigma_array[j], tau_array[i], 0.)
-    val = kelly_ll(t, X_data, err, sigma_array[j], tau_array[i], 0.)
+    val = kelly_ll([sigma_array[j], tau_array[i], b], t, X_data, err)
     ll.append(val)
-    Z[i][j]=val
   max_val=max(max_val,max(ll))
   if(j<=6): semilogx(tau_array,ll, '+-',lw=2, label='$\sigma$=%1.2e'%(sigma_array[j]))
   if(j>6): semilogx(tau_array,ll, '+--', lw=3, label='$\sigma$=%1.2e'%(sigma_array[j]))
-  ylim(-max_val,max_val)
+  ylim(-10.*max_val,2.*max_val)
   xlabel('tau, days')
-legend(loc=3)
+  ylabel('log likelihood')
+  title('True sigma=%1.2e'%(sigma))
+legend(loc=4)
+#show()
+
+tau_array=10**arange(-5.,5.,0.1)
+sigma_array=10**arange(-3,3,0.1)
+print 'len(X)',len(X)
+Ntau=len(tau_array)
+Nsig=len(sigma_array)
+Xg, Yg=np.meshgrid(log10(array(sigma_array)), log10(array(tau_array)))
+Z = Xg*Yg
+figure()
+max_val=0.
+for j in range(0,Nsig):
+  ll=[]
+  for i in range(0,Ntau):
+    #val = hojjati_ll(t, X_data, err, sigma_array[j], tau_array[i], 0.)
+    val = kelly_ll([sigma_array[j], tau_array[i], b], t, X_data, err)
+    ll.append(val)
+    Z[i][j]=val
+  max_val=max(max_val,max(ll))
+  #if(j<=6): semilogx(tau_array,ll, '+-',lw=2, label='$\sigma$=%1.2e'%(sigma_array[j]))
+  #if(j>6): semilogx(tau_array,ll, '+--', lw=3, label='$\sigma$=%1.2e'%(sigma_array[j]))
+  #ylim(-max_val,max_val)
+  #xlabel('tau, days')
+#legend(loc=3)
+
 
 #Z=(Yg-0.1*Xg)
-figure(5)
+figure()
 #max_val=max(Z)
 for i in range(0,Ntau):
   for j in range(0,Nsig):
-   if(Z[i][j]<-max_val): Z[i][j]=-max_val
+   if(Z[i][j]<-10.*max_val): Z[i][j]=-10*max_val
 #delta = 0.025
 #Z = bivariate_normal(10**X, 10**Y, 1.0, 1.0, 0.0, 0.0)
 #Z2 = mlab.bivariate_normal(X, Y, 1.5, 0.5, 1, 1)
@@ -147,10 +160,12 @@ plot([log10(tau)],[log10(sigma)],'wo', mew=2, mfc='none', mec='w')
 ylim(min(log10(sigma_array)),max(log10(sigma_array)))
 xlim(min(log10(tau_array)),max(log10(tau_array)))
 xlabel(r'$\log_{10}(\tau/$days$)$')
-ylabel(r'$\log_{10}(\sigma/$arb. u.$)$')
+ylabel(r'$\log_{10}(\sigma/$mag day$^{-1/2})$')
 #title('Hojjati Likelihood\nfor One Quasar Light Curve')
 title('Kelly Likelihood\nfor One Quasar Light Curve')
 #clabel(CS, inline=1, fontsize=10)
 #title('Simplest default with labels')
+
+
 
 show()
--- python/qsr_ll.py
+++ python/qsr_ll.py
@@ -1,6 +1,36 @@
 # -*- coding: utf-8 -*-
-from pylab import *
+#from pylab import *
 import numpy as np
+
+################################################################################################
+
+def kelly_ll(theta, time_array, flux_array, ph_err_array):
+  #see Equations 6-12 of Kelly, 2009
+  if(len(theta)!=3):
+    print 'qsr_ll.py'
+    print 'USAGE:***********************************************************************' 
+    print 'kelly_ll ( [sigma, tau, b], time_array, flux_array, measurement_error_array )'
+    print 'exiting'
+    exit()
+  sig, tau, b = theta
+  x=flux_array
+  t=time_array
+  ph_err_array*=0.
+  x_star=x-b*tau
+  x_hat=[0.]
+  Omega=[tau*sig**2/2.]
+  a=[0.]
+  ll=-(x_hat[0] - x_star[0])**2/(Omega[0]+ph_err_array[0]**2) - np.log(2*np.pi*(Omega[0]+ph_err_array[0]**2))
+  #print sig, tau
+  for i in range(1,len(t)):
+    x_star[i]=x[i]-b*tau
+    a.append(np.exp(-abs(t[i]-t[i-1])/tau))
+    Omega.append(Omega[0]*(1-a[i]**2) + a[i]**2*Omega[i-1]*(1.-Omega[i-1]/(Omega[i-1]+ph_err_array[i-1]**2)))
+    x_hat.append(a[i]*x_hat[i-1] + a[i]*Omega[i-1]/(Omega[i-1]+ph_err_array[i-1]**2)*(x_star[i-1]-x_hat[i-1]))
+    ll += -(x_hat[i] - x_star[i])**2/(Omega[i]+ph_err_array[i]**2) - np.log(2*np.pi*(Omega[i]+ph_err_array[i]**2))
+  return ll
+
+################################################################################################
 
 def hojjati_ll(time_array, flux_array, ph_err_array, sig, tau, b):
   #ll=-999.
@@ -29,28 +59,8 @@ def hojjati_ll(time_array, flux_array, ph_err_array, sig, tau, b):
   #print ''
   M_inv = np.linalg.inv(M)
   #if(M_det!=0. and M_det==M_det):
-  ll=-array(matrix(flux_array[0:Nsamp])*matrix(M_inv)*matrix(flux_array[0:Nsamp]).T)[0][0]-log_M_det-Nsamp*log(2.*pi)
+  ll=-array(matrix(flux_array[0:Nsamp])*matrix(M_inv)*matrix(flux_array[0:Nsamp]).T)[0][0]-log_M_det-Nsamp*log(2.*np.pi)
     #ll=-array(matrix(flux_array[0:Nsamp])*matrix(M_inv)*matrix(flux_array[0:Nsamp]).T)[0][0]
     #ll=-log(M_det)-Nsamp*log(2.*pi)
   return ll
   
-################################################################################################
-
-def kelly_ll(time_array, flux_array, ph_err_array, sig, tau, b):
-  #see Equations 6-12 of Kelly, 2009
-  x=flux_array
-  t=time_array
-  ph_err_array*=0.
-  x_star=x-b*tau
-  x_hat=[0.]
-  Omega=[tau*sig**2/2.]
-  a=[0.]
-  ll=-(x_hat[0] - x_star[0])**2/(Omega[0]+ph_err_array[0]**2) - log(2*pi*(Omega[0]+ph_err_array[0]**2))
-  print sig, tau
-  for i in range(1,len(t)):
-    x_star[i]=x[i]-b*tau
-    a.append(exp(-abs(t[i]-t[i-1])/tau))
-    Omega.append(Omega[0]*(1-a[i]**2) + a[i]**2*Omega[i-1]*(1.-Omega[i-1]/(Omega[i-1]+ph_err_array[i-1]**2)))
-    x_hat.append(a[i]*x_hat[i-1] + a[i]*Omega[i-1]/(Omega[i-1]+ph_err_array[i-1]**2)*(x_star[i-1]-x_hat[i-1]))
-    ll += -(x_hat[i] - x_star[i])**2/(Omega[i]+ph_err_array[i]**2) - log(2*pi*(Omega[i]+ph_err_array[i]**2))
-  return ll
