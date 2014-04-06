b63a59074f1cacd26b92a650616d7ce91e3e7b3e
Leonidas Moustakas <lmoustakas@gmail.com>
2014-04-05 22:09:25 -0700

    fixed shell script so that multiple cpus will be used...

--- python/nisusrun_140405.sh
+++ python/nisusrun_140405.sh
@@ -1,31 +1,31 @@
-./fot_delay.py -i 'RXJ1131_Tewes2013.rdb' -l 'A' -m 'B' -s 0.0 -o 'RXJ1131AB'
-./fot_delay.py -i 'RXJ1131_Tewes2013.rdb' -l 'A' -m 'C' -s 0.0 -o 'RXJ1131AC'
-./fot_delay.py -i 'RXJ1131_Tewes2013.rdb' -l 'B' -m 'C' -s 0.0 -o 'RXJ1131BC'
-./fot_delay.py -i 'RXJ1131_Tewes2013.rdb' -l 'A' -m 'D' -s 0.0 -o 'RXJ1131AD'
-./fot_delay.py -i 'RXJ1131_Tewes2013.rdb' -l 'B' -m 'D' -s 0.0 -o 'RXJ1131BD'
-./fot_delay.py -i 'RXJ1131_Tewes2013.rdb' -l 'C' -m 'D' -s 0.0 -o 'RXJ1131CD'
-./fot_delay.py -i 'HE0435_Courbin2011.rdb' -l 'A' -m 'B' -s 0.0 -o 'HE0435AB'
-./fot_delay.py -i 'HE0435_Courbin2011.rdb' -l 'A' -m 'C' -s 0.0 -o 'HE0435AC'
-./fot_delay.py -i 'HE0435_Courbin2011.rdb' -l 'B' -m 'C' -s 0.0 -o 'HE0435BC'
-./fot_delay.py -i 'HE0435_Courbin2011.rdb' -l 'A' -m 'D' -s 0.0 -o 'HE0435AD'
-./fot_delay.py -i 'HE0435_Courbin2011.rdb' -l 'B' -m 'D' -s 0.0 -o 'HE0435BD'
-./fot_delay.py -i 'HE0435_Courbin2011.rdb' -l 'C' -m 'D' -s 0.0 -o 'HE0435CD'
-./fot_delay.py -i 'HS2209_Eulaers2013.rdb' -l 'A' -m 'B' -s 0.0 -o 'HS2209AB'
-./fot_delay.py -i 'J1001_Rathnakumar2013.rdb' -l 'A' -m 'B' -s 0.0 -o 'J10019AB'
-./fot_delay.py -i 'J1206_Eulaers2013.rdb' -l 'A' -m 'B' -s 0.0 -o 'J1206AB'
+./fot_delay.py -i 'RXJ1131_Tewes2013.rdb' -l 'A' -m 'B' -s 0.0 -o 'RXJ1131AB' > RXJ1131AB.log & 
+./fot_delay.py -i 'RXJ1131_Tewes2013.rdb' -l 'A' -m 'C' -s 0.0 -o 'RXJ1131AC' > RXJ1131AC.log & 
+./fot_delay.py -i 'RXJ1131_Tewes2013.rdb' -l 'B' -m 'C' -s 0.0 -o 'RXJ1131BC' > RXJ1131BC.log & 
+./fot_delay.py -i 'RXJ1131_Tewes2013.rdb' -l 'A' -m 'D' -s 0.0 -o 'RXJ1131AD' > RXJ1131AD.log & 
+./fot_delay.py -i 'RXJ1131_Tewes2013.rdb' -l 'B' -m 'D' -s 0.0 -o 'RXJ1131BD' > RXJ1131BD.log & 
+./fot_delay.py -i 'RXJ1131_Tewes2013.rdb' -l 'C' -m 'D' -s 0.0 -o 'RXJ1131CD' > RXJ1131CD.log & 
+./fot_delay.py -i 'HE0435_Courbin2011.rdb' -l 'A' -m 'B' -s 0.0 -o 'HE0435AB' > HE0435AB.log & 
+./fot_delay.py -i 'HE0435_Courbin2011.rdb' -l 'A' -m 'C' -s 0.0 -o 'HE0435AC' > HE0435AC.log & 
+./fot_delay.py -i 'HE0435_Courbin2011.rdb' -l 'B' -m 'C' -s 0.0 -o 'HE0435BC' > HE0435BC.log & 
+./fot_delay.py -i 'HE0435_Courbin2011.rdb' -l 'A' -m 'D' -s 0.0 -o 'HE0435AD' > HE0435AD.log & 
+./fot_delay.py -i 'HE0435_Courbin2011.rdb' -l 'B' -m 'D' -s 0.0 -o 'HE0435BD' > HE0435BD.log & 
+./fot_delay.py -i 'HE0435_Courbin2011.rdb' -l 'C' -m 'D' -s 0.0 -o 'HE0435CD' > HE0435CD.log & 
+./fot_delay.py -i 'HS2209_Eulaers2013.rdb' -l 'A' -m 'B' -s 0.0 -o 'HS2209AB' > HS2209AB.log & 
+./fot_delay.py -i 'J1001_Rathnakumar2013.rdb' -l 'A' -m 'B' -s 0.0 -o 'J10019AB' > J10019AB.log & 
+./fot_delay.py -i 'J1206_Eulaers2013.rdb' -l 'A' -m 'B' -s 0.0 -o 'J1206AB' > J1206AB.log & 
 # now with a systematic floor of 0.1 magnitudes
-./fot_delay.py -i 'RXJ1131_Tewes2013.rdb' -l 'A' -m 'B' -s 0.1 -o 'RXJ1131AB_0p1'
-./fot_delay.py -i 'RXJ1131_Tewes2013.rdb' -l 'A' -m 'C' -s 0.1 -o 'RXJ1131AC_0p1'
-./fot_delay.py -i 'RXJ1131_Tewes2013.rdb' -l 'B' -m 'C' -s 0.1 -o 'RXJ1131BC_0p1'
-./fot_delay.py -i 'RXJ1131_Tewes2013.rdb' -l 'A' -m 'D' -s 0.1 -o 'RXJ1131AD_0p1'
-./fot_delay.py -i 'RXJ1131_Tewes2013.rdb' -l 'B' -m 'D' -s 0.1 -o 'RXJ1131BD_0p1'
-./fot_delay.py -i 'RXJ1131_Tewes2013.rdb' -l 'C' -m 'D' -s 0.1 -o 'RXJ1131CD_0p1'
-./fot_delay.py -i 'HE0435_Courbin2011.rdb' -l 'A' -m 'B' -s 0.1 -o 'HE0435AB_0p1'
-./fot_delay.py -i 'HE0435_Courbin2011.rdb' -l 'A' -m 'C' -s 0.1 -o 'HE0435AC_0p1'
-./fot_delay.py -i 'HE0435_Courbin2011.rdb' -l 'B' -m 'C' -s 0.1 -o 'HE0435BC_0p1'
-./fot_delay.py -i 'HE0435_Courbin2011.rdb' -l 'A' -m 'D' -s 0.1 -o 'HE0435AD_0p1'
-./fot_delay.py -i 'HE0435_Courbin2011.rdb' -l 'B' -m 'D' -s 0.1 -o 'HE0435BD_0p1'
-./fot_delay.py -i 'HE0435_Courbin2011.rdb' -l 'C' -m 'D' -s 0.1 -o 'HE0435CD_0p1'
-./fot_delay.py -i 'HS2209_Eulaers2013.rdb' -l 'A' -m 'B' -s 0.1 -o 'HS2209AB_0p1'
-./fot_delay.py -i 'J1001_Rathnakumar2013.rdb' -l 'A' -m 'B' -s 0.1 -o 'J10019AB_0p1'
-./fot_delay.py -i 'J1206_Eulaers2013.rdb' -l 'A' -m 'B' -s 0.1 -o 'J1206AB_0p1'
+./fot_delay.py -i 'RXJ1131_Tewes2013.rdb' -l 'A' -m 'B' -s 0.1 -o 'RXJ1131AB_0p1' > RXJ1131AB_0p1.log & 
+./fot_delay.py -i 'RXJ1131_Tewes2013.rdb' -l 'A' -m 'C' -s 0.1 -o 'RXJ1131AC_0p1' > RXJ1131AC_0p1.log & 
+./fot_delay.py -i 'RXJ1131_Tewes2013.rdb' -l 'B' -m 'C' -s 0.1 -o 'RXJ1131BC_0p1' > RXJ1131BC_0p1.log & 
+./fot_delay.py -i 'RXJ1131_Tewes2013.rdb' -l 'A' -m 'D' -s 0.1 -o 'RXJ1131AD_0p1' > RXJ1131AD_0p1.log & 
+./fot_delay.py -i 'RXJ1131_Tewes2013.rdb' -l 'B' -m 'D' -s 0.1 -o 'RXJ1131BD_0p1' > RXJ1131BD_0p1.log & 
+./fot_delay.py -i 'RXJ1131_Tewes2013.rdb' -l 'C' -m 'D' -s 0.1 -o 'RXJ1131CD_0p1' > RXJ1131CD_0p1.log & 
+./fot_delay.py -i 'HE0435_Courbin2011.rdb' -l 'A' -m 'B' -s 0.1 -o 'HE0435AB_0p1' > HE0435AB_0p1.log & 
+./fot_delay.py -i 'HE0435_Courbin2011.rdb' -l 'A' -m 'C' -s 0.1 -o 'HE0435AC_0p1' > HE0435AC_0p1.log & 
+./fot_delay.py -i 'HE0435_Courbin2011.rdb' -l 'B' -m 'C' -s 0.1 -o 'HE0435BC_0p1' > HE0435BC_0p1.log & 
+./fot_delay.py -i 'HE0435_Courbin2011.rdb' -l 'A' -m 'D' -s 0.1 -o 'HE0435AD_0p1' > HE0435AD_0p1.log & 
+./fot_delay.py -i 'HE0435_Courbin2011.rdb' -l 'B' -m 'D' -s 0.1 -o 'HE0435BD_0p1' > HE0435BD_0p1.log & 
+./fot_delay.py -i 'HE0435_Courbin2011.rdb' -l 'C' -m 'D' -s 0.1 -o 'HE0435CD_0p1' > HE0435CD_0p1.log & 
+./fot_delay.py -i 'HS2209_Eulaers2013.rdb' -l 'A' -m 'B' -s 0.1 -o 'HS2209AB_0p1' > HS2209AB_0p1.log & 
+./fot_delay.py -i 'J1001_Rathnakumar2013.rdb' -l 'A' -m 'B' -s 0.1 -o 'J10019AB_0p1' > J10019AB_0p1.log & 
+./fot_delay.py -i 'J1206_Eulaers2013.rdb' -l 'A' -m 'B' -s 0.1 -o 'J1206AB_0p1' > J1206AB_0p1.log & 
