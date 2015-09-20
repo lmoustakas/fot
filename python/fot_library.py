3d2d7ced1af20e31a9427e6797ce1a46c53c0faa
afromero <rw.andres@gmail.com>
2015-09-20 15:10:13 -0700

    factor of 2 was corrected

--- python/fot_library.py
+++ python/fot_library.py
@@ -137,7 +137,8 @@ def kelly_ll(theta, time_array, flux_array, ph_err_array):
   x=flux_array
   t=time_array
   x_hat, err = kelly_estimates(theta, time_array, flux_array, ph_err_array)
-  ll = np.sum( -((x_hat-x_star)**2 / (err**2)) - np.log(2*np.pi*(err**2)))  #ph_err_array*=0.
+  x_star=x-avg_mag
+  ll = -0.5 * np.sum( ((x_hat-x_star)**2 / (err**2)) + np.log(2*np.pi*(err**2))) 
   return ll
 
 def hojjati_ll(time_array, flux_array, ph_err_array, sig, tau, b):
@@ -297,7 +298,8 @@ def emcee_lightcurve_estimator(t, lc1, e1, output_tag,
   ylabel('magnitude')
   legend(loc=1)
 
-    
+  if(outputdir[-1]!='/'):
+	outputdir = outputdir+'/'        
   fig.savefig(outputdir+"burn_in_triangle_%s.png"%(output_tag))
   
 
@@ -518,7 +520,8 @@ def emcee_delay_estimator(t, lc1, e1, lc2, e2, output_tag,
   ylabel('magnitude')
   legend(loc=1)
 
-    
+  if(outputdir[-1]!='/'):
+	outputdir = outputdir+'/'    
   fig.savefig(outputdir+"burn_in_triangle_%s.png"%(output_tag))
   print outputdir+"burn_in_triangle_%s.png"%(output_tag)
 
--- python/lcogt_run_prep.py
+++ python/lcogt_run_prep.py
@@ -1,14 +1,14 @@
 import numpy as np
 
-outdir = '/disk4/romerowo/fot_lcogt_outputs/20150919/'
+outdir = '/disk4/romerowo/fot_lcogt_outputs/20150920'
 
 fout = open('fot_lcogt_runs.txt', 'w')
 for img in ['A', 'B', 'C', 'D']:
-	fout.write('./fot_lcogt_lc.py -i /nisushome/romerowo/lcolens/python/arw/he0435-1223_lcogt_magnitudes.dat -im  %s -o HE0435_LC_img_%s -od %s > /disk4/romerowo/fot_lcogt_outputs/20150919/HE0435_LC_img_%s.log'%(img, img, outdir, img)+'\n')
+	fout.write('./fot_lcogt_lc.py -i /nisushome/romerowo/lcolens/python/arw/he0435-1223_lcogt_magnitudes.dat -im  %s -o HE0435_LC_img_%s -od %s > %s/HE0435_LC_img_%s.log'%(img, img, outdir, outdir, img)+'\n')
 
 for delay in np.arange(-2.0,2.1, 0.1):
 	tag = 'lcogt_dAB_dt_%1.1f'%(delay)
-	com = './fot_lcogt_delay.py -i /nisushome/romerowo/lcolens/python/arw/he0435-1223_lcogt_magnitudes.dat -im1 A -im2 B -dtp %1.1f -dtpmin %1.1f -dtpmax %1.1f -sp 0.25 -spmin 0.01 -spmax 1.0 -tp 160. -tpmin 5. -tpmax 1000. -o %s -od %s > /disk4/romerowo/fot_lcogt_outputs/20150919/%s.log'%(delay, delay-0.1, delay+0.1, tag, outdir, tag)
+	com = './fot_lcogt_delay.py -i /nisushome/romerowo/lcolens/python/arw/he0435-1223_lcogt_magnitudes.dat -im1 A -im2 B -dtp %1.1f -dtpmin %1.1f -dtpmax %1.1f -sp 0.25 -spmin 0.01 -spmax 1.0 -tp 160. -tpmin 5. -tpmax 1000. -o %s -od %s > %s/%s.log'%(delay, delay-0.1, delay+0.1, tag, outdir, outdir, tag)
 	fout.write(com+'\n')
 
 fout.close()
